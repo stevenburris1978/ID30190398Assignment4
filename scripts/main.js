186a6f6c9d587cef54080641b76434a9469e4b27
stevenburris1978 <sburris@mail.fhsu.edu>
2022-10-03 23:39:49 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,80 @@
+/* Practice Assignment 4:
+
+Complete this javascript file according to the individual instructions
+given in the comments. 
+
+*** DO NOT CHANGE any of the code that you are not instructed to. */
+
+// 1) Create a function named helloWorld that returns the exact phrase:
+// "Hello World!".
+
+function helloWorld() {
+    const a = 'Hello World!';
+    document.write('Question 1. ' + (a) + '<br>');
+    console.log(a);
+}
+helloWorld();
+
+// 2) Create a function named greeting that will accept a "name" parameter
+// and return the string: "Hello, name." where "name" is replaced
+// with whatever value we want to provide when calling the function.
+
+function greeting(name) {
+    return 'Hello, ' + name + '.';
+}
+document.write(' Question 2. ' + greeting('Danny') + '<br>');
+console.log(greeting('Steve'));
+
+// 3) Create a function named divisibleByThree that accepts a "number" parameter
+// and will return accurate boolean data depending on the value
+// of the "number" parameter. Hint: Try the modulus operator.
+
+function divisibleByThree(number) {
+    if (number%3) {
+        return true;
+    } else {
+        return false;
+    }   
+}
+document.write(' Question 3. ' + divisibleByThree(9) + '<br>');
+console.log(divisibleByThree(10));
+
+// 4) Create a function named averageAge that accepts 3 parameters:
+// num1, num2, and num3. The function should return the average of
+// all three numbers, but the parameter num3 is missing, it should
+// return the average of num1 and num2. Returned results should be
+// rounded to the nearest integer. Hint: Use a Math method to round.
+
+function averageAge(num1,num2,num3) {
+    if (num3 !== undefined) {
+        return Math.round((num1 + num2 + num3)/3);
+    } else {
+        return Math.round((num1 + num2)/2);
+    }   
+}
+document.write(' Question 4. ' + averageAge(4,9) + '<br>');
+console.log(averageAge(4,8,19));
+
+// 5) Create a function named leetSpeak that accepts a word
+// as a parameter and returns the same word except all of the letter
+// e occurances (lower case only) are replaced by the number 3.
+// Replace any lower case a with the number 4, too.
+// Hints: You will use a loop. You will use conditionals.
+// Example: leet is returned as l33t. speak is returned as sp34k.
+
+function leetSpeak(word)
+{
+  let output = [];      
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == "a") {
+      output[i] = "4";
+    } else if (word[i] == "e") {
+      output[i] = "3";
+    }
+    else output[i] = word[i];
+  } 
+  output= output.join('');   
+  return output
+}
+document.write(' Question 5. ' + leetSpeak('leet') + '<br>');
+console.log(leetSpeak('speak'));
